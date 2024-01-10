5e554886a40ec76f535a98197963436ec9086ffb
mykola2312 <nikolay_tihonov@i.ua>
2024-01-10 22:27:27 +0200

    setup UART and libc stdio syscalls

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include "stm32f4xx_hal.h"
+#include "uart.h"
+#include <stdio.h>
 
 int main(void)
 {
   HAL_Init();
+  
+  UART_Init();
+
+  printf("Hello World!\r\n");
 
   while (1) {}
 }
---
+++ src/uart.c
@@ -0,0 +1,32 @@
+#include "uart.h"
+#include "stm32f4xx_hal.h"
+
+UART_HandleTypeDef huart1 = {
+  .Instance = USART1,
+  .Init.BaudRate = 115200,
+  .Init.WordLength = UART_WORDLENGTH_8B,
+  .Init.StopBits = UART_STOPBITS_1,
+  .Init.Parity = UART_PARITY_NONE,
+  .Init.Mode = UART_MODE_TX_RX,
+  .Init.HwFlowCtl = UART_HWCONTROL_NONE,
+  .Init.OverSampling = UART_OVERSAMPLING_16
+};
+
+// stdout/stdin syscalls for UART
+int __io_putchar(int ch)
+{
+  HAL_UART_Transmit(&huart1, (const uint8_t*)&ch, 1, -1);
+  return ch;
+}
+
+int __io_getchar(void)
+{
+  uint8_t ch = 0;
+  HAL_UART_Receive(&huart1, &ch, 1, -1);
+  return ch;
+}
+
+void UART_Init(void)
+{
+    HAL_UART_Init(&huart1);
+}
