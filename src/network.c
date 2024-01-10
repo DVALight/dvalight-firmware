e03d1e788086f5a911b3db2364b483acd43420ee
mykola2312 <nikolay_tihonov@i.ua>
2024-01-11 00:35:13 +0200

    implement mac/ip parsing functions

--- src/network.c
+++ src/network.c
@@ -15,10 +15,25 @@ SPI_HandleTypeDef hspi1 = {
   .Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE
 };
 
+uint8_t g_LocalMAC[6];
+uint8_t g_LocalIP[4];
+
 int NET_Init(void)
 {
   if (HAL_SPI_Init(&hspi1) != HAL_OK)
     return -1;
 
   return 0;
+}
+
+void NET_ParseMAC(uint8_t* mac, const char* macStr)
+{
+  sscanf(macStr, "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
+    &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
+}
+
+void NET_ParseIP(uint8_t* ip, const char* ipStr)
+{
+  sscanf(ipStr, "%hhd.%hhd.%hhd.%hhd",
+    &ip[0], &ip[1], &ip[2], &ip[3]);
 }
--- src/network.h
+++ src/network.h
@@ -1,6 +1,14 @@
 #ifndef __NETWORK_H
 #define __NETWORK_H
 
+#include <stdint.h>
+
+extern uint8_t g_LocalMAC[6];
+extern uint8_t g_LocalIP[4];
+
 int NET_Init(void);
+
+void NET_ParseMAC(uint8_t* mac, const char* macStr);
+void NET_ParseIP(uint8_t* ip, const char* ipStr);
 
 #endif
