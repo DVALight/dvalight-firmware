045fb957cf69ef4ffcc174c661c3cde1fcc55afa
mykola2312 <nikolay_tihonov@i.ua>
2024-01-11 08:16:37 +0200

    send UDP packets in defined interval of ticks

--- src/main.c
+++ src/main.c
@@ -27,8 +27,19 @@ int main(void)
     Failure_Hang_Loop();
   }
 
+  printf("Init OK\r\n");
+
+  uint32_t lastSendTick = HAL_GetTick();
+  const uint32_t sendDelayTicks = 1000;
+
   while (1)
   {
+    if (HAL_GetTick() > lastSendTick + sendDelayTicks)
+    {
+      NET_SendUDP(g_RemoteIP, 61337, 1337, (uint8_t*)"Hello World!\r\n", 15);
+      lastSendTick = HAL_GetTick();
+    }
+
     uint16_t pos = packetloop_icmp_tcp(g_NetBuf,
       ES_enc28j60PacketReceive(sizeof(g_NetBuf), g_NetBuf));
     if (!pos) continue;
