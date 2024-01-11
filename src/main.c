66ea924eeb827ac175643599e8420f4a3c4bc023
mykola2312 <nikolay_tihonov@i.ua>
2024-01-11 12:56:26 +0200

    seems like EXTI works fine

--- src/main.c
+++ src/main.c
@@ -13,12 +13,21 @@ void Failure_Hang_Loop(void)
   }
 }
 
-volatile uint8_t g_EncInt = 0;
+volatile uint8_t g_PktIF = 0;
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
   if (GPIO_Pin == GPIO_PIN_1)
   {
-    g_EncInt = 1;
+    enc28j60WriteOp(ENC28J60_BIT_FIELD_CLR, EIE, EIE_INTIE);
+
+    uint8_t eir = enc28j60ReadOp(ENC28J60_READ_CTRL_REG, EIR);
+    if (eir & EIR_PKTIF)
+    {
+      g_PktIF = 1;
+      enc28j60WriteOp(ENC28J60_BIT_FIELD_CLR, EIR, EIR_PKTIF);
+    }
+
+    enc28j60WriteOp(ENC28J60_BIT_FIELD_SET, EIE, EIE_INTIE);
   }
 }
 
@@ -31,56 +40,34 @@ int main(void)
     Failure_Hang_Loop();
   }
 
-  if (NET_Init())
-  {
-    printf("Failed to initialize network\r\n");
-    Failure_Hang_Loop();
-  }
-
   // EXTI1 line
   HAL_GPIO_Init(GPIOA, &(GPIO_InitTypeDef) {
     .Pin = GPIO_PIN_1,
     .Mode = GPIO_MODE_IT_FALLING,
     .Pull = GPIO_NOPULL
   });
-  // BUG: such priority could shadow SysTick interrupt causing SPI HAL not working
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 1, 1);
   HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+
+  if (NET_Init())
+  {
+    printf("Failed to initialize network\r\n");
+    Failure_Hang_Loop();
+  }
 
   printf("Init OK\r\n");
 
-  // uint32_t lastSendTick = HAL_GetTick();
-  // const uint32_t sendDelayTicks = 1000;
-
   while (1)
   {
-    if (g_EncInt)
+    if (g_PktIF)
     {
-      // disable enc interrupts
-      enc28j60WriteOp(ENC28J60_BIT_FIELD_CLR, EIE, EIE_INTIE);
+      printf("g_PktIf\r\n");
 
-      // uint8_t eir = enc28j60ReadOp(ENC28J60_READ_CTRL_REG, EIR);
-      // if (eir & EIR_PKTIF)
-      // {
-        printf("EXTI1: EIR_PKTIF - packet received\r\n");
-        // printf("EPKTCNT: %u\r\n", enc28j60Read(EPKTCNT));
-      // }
+      uint16_t pos = packetloop_icmp_tcp(g_NetBuf,
+        ES_enc28j60PacketReceive(sizeof(g_NetBuf), g_NetBuf));
+      printf("pos %u\r\n", pos);
 
-      // // enable enc interrupts
-      enc28j60WriteOp(ENC28J60_BIT_FIELD_SET, EIE, EIE_INTIE|EIE_PKTIE);
-
-      g_EncInt = 0;
+      g_PktIF = 0;
     }
-
-    // if (HAL_GetTick() > lastSendTick + sendDelayTicks)
-    // {
-    //   NET_SendUDP(g_RemoteIP, 61337, 1337, (uint8_t*)"Hello World!\r\n", 15);
-    //   lastSendTick = HAL_GetTick();
-    // }
-
-    // uint16_t pos = packetloop_icmp_tcp(g_NetBuf,
-    //   ES_enc28j60PacketReceive(sizeof(g_NetBuf), g_NetBuf));
-    // if (!pos) continue;
-    // printf("pos %u\n", pos);
   }
 }
