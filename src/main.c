5dd0a2db8bb50bf9d7d91aa669412d5904bc2b27
mykola2312 <nikolay_tihonov@i.ua>
2024-01-11 10:15:45 +0200

    implement EXTI1 interrupt

--- src/main.c
+++ src/main.c
@@ -2,14 +2,33 @@
 #include "stm32f4xx_hal.h"
 #include "network.h"
 #include "uart.h"
+#include "enc28j60.h"
 #include "EtherShield.h"
 #include "ip_arp_udp_tcp.h"
 
-void Failure_Hang_Loop()
+void Failure_Hang_Loop(void)
 {
   while (1) {
     __NOP();
   }
+}
+
+void EXTI1_IRQHandler(void)
+{
+  printf("EXTI1_IRQHandler\r\n");
+
+  // disable enc interrupts
+  enc28j60WriteOp(ENC28J60_BIT_FIELD_CLR, EIE, EIE_INTIE);
+
+  uint8_t eir = enc28j60ReadOp(ENC28J60_READ_CTRL_REG, EIR);
+  if (eir & EIR_PKTIF)
+  {
+    printf("EXTI1: EIR_PKTIF - packet received\r\n");
+    printf("EPKTCNT: %u\r\n", enc28j60Read(EPKTCNT));
+  }
+
+  // enable enc interrupts
+  enc28j60WriteOp(ENC28J60_BIT_FIELD_SET, EIE, EIE_INTIE);
 }
 
 int main(void)
@@ -26,6 +45,16 @@ int main(void)
     printf("Failed to initialize network\r\n");
     Failure_Hang_Loop();
   }
+
+  // EXTI1 line
+  HAL_GPIO_Init(GPIOA, &(GPIO_InitTypeDef) {
+    .Pin = GPIO_PIN_1,
+    .Mode = GPIO_MODE_IT_FALLING,
+    .Pull = GPIO_NOPULL
+  });
+  // BUG: such priority could shadow SysTick interrupt causing SPI HAL not working
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
 
   printf("Init OK\r\n");
 
